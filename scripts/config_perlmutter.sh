058e44687348c211ffac09ae556203e98a063c13
William F Godoy <williamfgc@yahoo.com>
2024-11-04 10:55:54 -0500

    Improve config_perlmutter.sh

--- scripts/config_perlmutter.sh
+++ scripts/config_perlmutter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Using a predefined $SCRATCH/$USER directory
+# This script is used to configure the GrayScott.jl project on NERSC's Perlmutter system.
+# Run it as `source GrayScott.jl/scripts/config_perlmutter.sh` to make changes permanent
+# Using a predefined $SCRATCH/$USER directory as top-level structure
+
+# Packages must be installed in the parallel filesystem 
+# and not user's home directory ~/.julia (default) which does not scale
+export JULIA_DEPOT_PATH=$SCRATCH/$USER/.julia
+
 GS_DIR=$SCRATCH/$USER/GrayScott.jl
-# remove existing generated Manifest.toml
+# remove existing generated Manifest.toml to make this script reusable
 rm -f $GS_DIR/Manifest.toml
 rm -f $GS_DIR/LocalPreferences.toml
 
@@ -11,8 +18,9 @@ module purge
 
 # load required modules
 module load PrgEnv-gnu
+# this is just due to an adios2 requirement, parallel HDF5 can be added in the future
 module load cray-hdf5-parallel
-#module load nvhpc
+# module load nvhpc not tested, use cudatoolkit instead
 module load cudatoolkit/12.2
 ml use /global/common/software/nersc/julia_hpc_24/modules
 module load adios2
@@ -21,7 +29,8 @@ module load julia/1.10.4
 # Required to point at underlying modules above
 export JULIA_ADIOS2_PATH=/global/common/software/nersc/julia_hpc_24/adios2/gnu
 
-# MPIPreferences to use Cray's MPICH
+# MPIPreferences.jl configures MPI.jl to use the underlying Cray's MPICH
+# It will populate LocalPreferences.toml with the correct MPI binary and libraries
 julia --project=$GS_DIR -e 'using Pkg; Pkg.add("MPIPreferences")'
 julia --project=$GS_DIR -e 'using MPIPreferences; MPIPreferences.use_system_binary(mpiexec="srun", vendor="cray")'
 
