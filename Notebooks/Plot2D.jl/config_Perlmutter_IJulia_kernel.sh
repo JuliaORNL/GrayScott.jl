cda0180be9a4dfb55a7168a1ddb5d2e4be89a5ca
William F Godoy <williamfgc@yahoo.com>
2024-11-16 21:38:09 -0500

    instantiate plots

--- Notebooks/Plot2D.jl/config_Perlmutter_IJulia_kernel.sh
+++ Notebooks/Plot2D.jl/config_Perlmutter_IJulia_kernel.sh
@@ -6,5 +6,8 @@ ml use /global/common/software/nersc/julia_hpc_24/modules
 module load adios2
 export JULIA_ADIOS2_PATH=/global/common/software/nersc/julia_hpc_24/adios2/gnu
 
+# instantiate project packages
+julia --project -e 'using Pkg; Pkg.instantiate()'
+
 # capture current environment
 julia --project -e 'using IJulia; installkernel("Julia-16-threads", "--project=@.", env=Dict("LD_LIBRARY_PATH" => string(ENV["LD_LIBRARY_PATH"]), "JULIA_NUM_THREADS" => "16", "JULIA_ADIOS2_PATH" => string(ENV["JULIA_ADIOS2_PATH"]) ))'
