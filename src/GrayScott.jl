c9c0be4b9df376eeccbd0cfc60cdb3c771ea5be8
tdehoff <tatianadehoff@gmail.com>
2025-06-11 10:40:44 -0400

    added bandwidth calculation

--- src/GrayScott.jl
+++ src/GrayScott.jl
@@ -1,6 +1,6 @@
 """
-GrayScott.jl is a Simulation and Analysis parallel framework for solving the 
-Gray-Scott 3D diffusion reaction system of equations of two variables U and V on 
+GrayScott.jl is a Simulation and Analysis parallel framework for solving the
+Gray-Scott 3D diffusion reaction system of equations of two variables U and V on
 a regular Cartesian mesh.
 
 The bp output files can be visualized with ParaView.
@@ -8,6 +8,8 @@ The bp output files can be visualized with ParaView.
 module GrayScott
 
 import MPI, ADIOS2
+
+using Statistics
 
 # contains relevant data containers "structs" for Input, Domain and Fields
 include(joinpath("simulation", "Structs.jl"))
@@ -59,26 +61,55 @@ function main(args::Vector{String})::Int32
     stream = IO.init(settings, mpi_cart_domain, fields)
 
     restart_step::Int32 = 0
-    # @TODO: checkpoint-restart 
+    # @TODO: checkpoint-restart
     step::Int32 = restart_step
 
-    while step < settings.steps
-        Simulation.iterate!(fields, settings, mpi_cart_domain)
+    # array to store times per iteration
+    elapsed_times = Float32[]
+
+    println("--------------------")
+    println("L = ", settings.L)
+    println("Block dimensions: ", settings.block_dim)
+
+    @time while step < settings.steps
+        Simulation.iterate!(fields, settings, mpi_cart_domain, elapsed_times)
         step += 1
 
         if step % settings.plotgap == 0
-            if rank == 0
-                println("Simulation at step ", step, " writing output step ",
-                    step / settings.plotgap)
-            end
+            # if rank == 0
+            #     println("Simulation at step ", step, " writing output step ",
+            #         step / settings.plotgap)
+            # end
 
             IO.write_step!(stream, step, fields)
         end
     end
 
+    # Compute the effective memory bandwidth:
+    # effective_memory_bandwidth = (theoretical_fetch_size + theoretical_write_size) / average_kernel_execution_time
+
+    println("Elapsed times size: ", length(elapsed_times))
+
+    # the warm-up call is not used for calculation
+    avg_kernel_execution = mean(elapsed_times[2:end])
+    println("avg kernel execution time (s): ", avg_kernel_execution)
+
+    n_xyz = settings.L
+    println("nx, ny, nz = ", n_xyz)
+    # theoretical_fetch_size = ((nx * ny * nz - 8 - 4 * (nx - 2) - 4 * (ny - 2) - 4 * (nz - 2) ) * sizeof(double)
+    fetch_size               = (n_xyz^3 - 8 - 12 * (n_xyz - 2) ) * sizeof(Float32)
+    println("theoretical_fetch_size = ", fetch_size)
+
+    # theoretical_write_size = ((nx - 2) * (ny - 2) * (nz - 2)) * sizeof(double)
+    write_size = ((n_xyz - 2) ^ 3) * sizeof(Float32)
+    println("theoretical_write_size = ", write_size)
+
+    memory_bandwidth = (fetch_size + write_size) / avg_kernel_execution
+    println("computed bandwidth = ", memory_bandwidth / 1e9, " GB/s")
+
     IO.close!(stream)
 
-    # Debugging session or Julia REPL session, not needed overall as it would be 
+    # Debugging session or Julia REPL session, not needed overall as it would be
     # called when the program ends
     if !isinteractive()
         MPI.Finalize()
