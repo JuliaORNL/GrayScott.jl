09d10f5aa58feb62590c09ab60c8e693ca0d303e
William F Godoy <williamfgc@yahoo.com>
2025-11-13 20:14:10 -0500

    Notebooks for SC25

--- Notebooks/Plot2D.jl/config_Perlmutter_IJulia_kernel.sh
+++ Notebooks/Plot2D.jl/config_Perlmutter_IJulia_kernel.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # source this file to generate a new IJulia kernel for Perlmutter
-module load julia/1.10.4
+module load PrgEnv-nvidia
+# this is just due to an adios2 requirement, parallel HDF5 can be added in the future
 module load cray-hdf5-parallel
-ml use /global/common/software/nersc/julia_hpc_24/modules
+# module load nvhpc not tested, use cudatoolkit instead
+module load cudatoolkit/12.2
+module use /global/common/software/nstaff/blaschke/tutorials/julia-hpc-tutorial-icpp25/nersc/modules
 module load adios2
-export JULIA_ADIOS2_PATH=/global/common/software/nersc/julia_hpc_24/adios2/gnu
+export JULIA_ADIOS2_PATH=/global/common/software/nstaff/blaschke/tutorials/julia-hpc-tutorial-icpp25/nersc/adios2/install/nvidia
+
+module load julia/1.12.1
 
 # instantiate project packages
 julia --project -e 'using Pkg; Pkg.instantiate()'
